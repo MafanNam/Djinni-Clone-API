a32b36aa0f45d2fa24e756e99ade05d877370543
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:20 +0200

    improve

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -8,7 +8,6 @@ env = environ.Env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-APP_DIR = BASE_DIR / "apps"
 
 DEBUG = env("DEBUG", default=False)
 
@@ -137,120 +136,9 @@ CORS_URLS_REGEX = r"^api/.*$"
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
-MEDIA_URL = "/mediafiles/"
-MEDIA_ROOT = BASE_DIR / "mediafiles"
-
-# Default primary key field type
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-# DEFAULT USER MODEL
-
-AUTH_USER_MODEL = "users.User"
-
-USER_ONLINE_TIMEOUT = env("USER_ONLINE_TIMEOUT", default=300)
-USER_LAST_LOGIN_EXPIRE = env("USER_LAST_LOGIN_EXPIRE", default=60 * 60 * 24 * 7)
-
-# CELERY
-CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="redis://localhost:6379/0")
-CELERY_ACCEPT_CONTENT = ["application/json"]
-CELERY_TASK_SERIALIZER = "json"
-CELERY_RESULT_SERIALIZER = "json"
-CELERY_RESULT_BACKEND_MAX_RETRIES = 10
-CELERY_TASK_SEND_SENT_EVENT = True
-CELERY_TASK_TRACK_STARTED = True
-CELERY_TASK_TIME_LIMIT = 30 * 60
-
-if USE_TZ:
-    CELERY_TIMEZONE = TIME_ZONE
-
-# CELERY RESULT SETTINGS
-CELERY_RESULT_EXTENDED = True
-CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND", default="django-db")
-CELERY_CACHE_BACKEND = env("CELERY_CACHE_BACKEND", default="django-cache")
-
-# CELERY BEAT SETTINGS
-CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
-
-# CACHE
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": env("REDIS_CACHE_LOCATION", default="redis://localhost:6379/1"),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
-    }
-}
-
-# DOC
-SPECTACULAR_SETTINGS = {
-    "TITLE": "Djinni Clone API",
-    "DESCRIPTION": "",
-    "VERSION": "0.0.0",
-    "SERVE_INCLUDE_SCHEMA": False,
-    "COMPONENT_SPLIT_REQUEST": True,
-    "SCHEMA_PATH_PREFIX": r"/api/v1/",
-}
-
-REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": ("rest_framework_simplejwt.authentication.JWTAuthentication",),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
-    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
-}
-
-AUTHENTICATION_BACKENDS = (
-    "social_core.backends.google.GoogleOAuth2",
-    "django.contrib.auth.backends.ModelBackend",
-)
-
-SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("Bearer",),
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
-    "ROTATE_REFRESH_TOKENS": True,
-    "SIGNING_KEY": env("SIGNING_KEY", default=""),
-    "USER_ID_FIELD": "id",
-    "USER_ID_CLAIM": "user_id",
-    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
-    "AUTH_COOKIE": "access_token",
-}
-
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
-
-DJOSER = {
-    "LOGIN_FIELD": "email",
-    "USER_CREATE_PASSWORD_RETYPE": True,
-    "USERNAME_CHANGED_EMAIL_CONFIRMATION": True,
-    "PASSWORD_CHANGED_EMAIL_CONFIRMATION": True,
-    "SEND_CONFORMATION_EMAIL": True,
-    "SET_USERNAME_RETYPE": True,
-    "SET_PASSWORD_RETYPE": True,
-    "PASSWORD_RESET_CONFIRM_URL": "password/reset/confirm/{uid}/{token}",
-    "USERNAME_RESET_CONFIRM_URL": "email/reset/confirm/{uid}/{token}",
-    "ACTIVATION_URL": "activate/{uid}/{token}",
-    "SEND_ACTIVATION_EMAIL": True,
-    "SOCIAL_AUTH_TOKEN_STRATEGY": "djoser.social.token.jwt.TokenStrategy",
-    "SOCIAL_AUTH_ALLOWED_REDIRECT_URIS": env.list(
-        "SOCIAL_AUTH_ALLOWED_REDIRECT_URIS", default=["http://localhost:8000", "http://localhost:3000"]
-    ),
-    "SERIALIZERS": {
-        "user_create_password_retype": "apps.users.api.serializers.CustomUserCreatePasswordRetypeSerializer",
-        "user": "apps.users.api.serializers.CustomUserSerializer",
-        "current_user": "apps.users.api.serializers.CustomUserSerializer",
-        "user_delete": "djoser.serializers.UserDeleteSerializer",
-    },
-    "PASSWORD_RESET_SHOW_EMAIL_NOT_FOUND": True,
-    "USERNAME_RESET_SHOW_EMAIL_NOT_FOUND": True,
-}
-
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = env("SOCIAL_AUTH_GOOGLE_OAUTH2_KEY", default="")
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = env("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET", default="")
-SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
-    "https://www.googleapis.com/auth/userinfo.email",
-    "https://www.googleapis.com/auth/userinfo.profile",
-    "openid",
+STATICFILES_FINDERS = [
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
-SOCIAL_AUTH_GOOGLE_OAUTH2_EXTRA_DATA = ["first_name", "last_name"]
+
+MEDIA_
