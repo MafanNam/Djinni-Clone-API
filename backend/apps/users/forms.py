e9d37557b4d509e0ffd0febfb24a9d0bafc95e29
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:15 +0200

    improve

--- backend/apps/users/forms.py
+++ backend/apps/users/forms.py
@@ -1,16 +1,11 @@
 from django import forms
-from django.contrib.auth import forms as admin_forms, get_user_model
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 User = get_user_model()
 
-
-class UserChangeForm(admin_forms.UserChangeForm):
-    class Meta(admin_forms.UserChangeForm.Meta):
-        model = User
-
-
-class UserCreationForm(admin_forms.UserCreationForm):
-    class Meta(admin_forms.UserCreationForm.Meta):
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
         model = User
         fields = ("first_name", "last_name", "email")
 
@@ -20,6 +15,11 @@ class UserCreationForm(admin_forms.UserCreationForm):
 
     def clean_email(self):
         email = self.cleaned_data["email"]
-        if User.objects.filter(email=email).exists():
+        if User.objects.filter(email__iexact=email).exists():
             raise forms.ValidationError(self.error_messages["duplicate_email"])
         return email
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = User
+
