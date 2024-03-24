2c8df50dfca42b8de7772ab901ad8a193a8d011b
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:03 +0200

    improve

--- backend/apps/other/admin.py
+++ backend/apps/other/admin.py
@@ -1,18 +1,16 @@
 from apps.other.models import Category, Company
 from django.contrib import admin
 
+class MyModelAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+    list_per_page = 20
 
 @admin.register(Company)
-class CompanyAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
-    list_display_links = ("id", "name")
-    search_fields = ("name",)
-    list_per_page = 20
-
+class CompanyAdmin(MyModelAdmin):
+    pass
 
 @admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ("id", "name")
-    list_display_links = ("id", "name")
-    search_fields = ("name",)
-    list_per_page = 20
+class CategoryAdmin(MyModelAdmin):
+    pass
