44fb4b75185427f4167b58f7b855abdb7bf3600e
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:06 +0200

    improve

--- backend/apps/vacancy/apps.py
+++ backend/apps/vacancy/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
+import apps.vacancy.signals  # import signals at the top of the file
 from django.utils.translation import gettext_lazy as _
-
 
 class VacancyConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -8,4 +8,5 @@ class VacancyConfig(AppConfig):
     verbose_name = _("Vacancy")
 
     def ready(self):
-        import apps.vacancy.signals
+        # Connect signals in the ready method
+        apps.vacancy.signals.connect_signals()
