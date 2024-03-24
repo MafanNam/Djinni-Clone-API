fcca463ecfd5a59a302879dc5b9ba3bdacf826b3
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:09 +0200

    improve

--- backend/apps/vacancy/api/urls.py
+++ backend/apps/vacancy/api/urls.py
@@ -2,12 +2,17 @@ from django.urls.conf import path
 
 from . import views
 
-urlpatterns = [
-    path("", views.VacancyListCreateAPIView.as_view(), name="vacancy_list"),
-    # path("candidate/feedbacks/", views.CandidateFeedbackListAPIView.as_view(), name="candidate_feedback"),
-    # path(
-    #     "candidate/feedbacks/<int:pk>", views.CandidateFeedbackDetailAPIView.as_view(), name="candidate_feedback_detail"
-    # ),
-    path("<slug:slug>/", views.VacancyDetailAPIView.as_view(), name="vacancy_detail"),
-    path("<slug:slug>/feedbacks/", views.FeedbackListCreateAPIView.as_view(), name="vacancy_feedback_list"),
+company_urlpatterns = [
+    path("", views.VacancyListCreateAPIView.as_view(), name="company_vacancy_list"),
+    path("<slug:slug>/", views.VacancyDetailAPIView.as_view(), name="company_vacancy_detail"),
+    path("<slug:slug>/feedbacks/", views.FeedbackListCreateAPIView.as_view(), name="company_vacancy_feedback_list"),
 ]
+
+candidate_urlpatterns = [
+    path("feedbacks/", views.CandidateFeedbackListAPIView.as_view(), name="candidate_feedback_list"),
+    path(
+        "feedbacks/<int:pk>", views.CandidateFeedbackDetailAPIView.as_view(), name="candidate_feedback_detail"
+    ),
+]
+
+urlpatterns = company_urlpatterns + candidate_urlpatterns
