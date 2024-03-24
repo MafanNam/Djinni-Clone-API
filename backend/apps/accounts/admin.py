6d3fbb85533f9e71fd5ae6facf631b4b23c0b739
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:02 +0200

    improve

--- backend/apps/accounts/admin.py
+++ backend/apps/accounts/admin.py
@@ -1,32 +1,29 @@
 from apps.accounts.models import CandidateProfile, ContactCv, RecruiterProfile
 from django.contrib import admin
 
-
 @admin.register(CandidateProfile)
 class CandidateProfileAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "first_name", "last_name", "skill_list")
-    list_display_links = ("id", "user")
-    search_fields = ("first_name", "last_name")
+    list_display = ("candidate_id", "candidate_user", "candidate_first_name", "candidate_last_name", "candidate_skills")
+    list_display_links = ("candidate_id", "candidate_user")
+    search_fields = ("candidate_first_name", "candidate_last_name")
     list_filter = ("user", "created_at", "updated_at")
 
     def get_queryset(self, request):
         return super().get_queryset(request).prefetch_related("skills")
 
-    def skill_list(self, obj):
+    def candidate_skills(self, obj):
         return ", ".join(skill.name for skill in obj.skills.all())
 
 
-@admin.register(RecruiterProfile)
-class RecruiterProfileAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "first_name", "last_name")
-    list_display_links = ("id", "user")
-    search_fields = ("first_name", "last_name")
-    list_filter = ("user", "created_at", "updated_at")
+models_to_register = (
+    (RecruiterProfile, "recruiter_", ("user", "first_name", "last_name")),
+    (ContactCv, "contact_", ("user", "first_name", "last_name", "created_at")),
+)
 
-
-@admin.register(ContactCv)
-class ContactCvAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "first_name", "last_name", "created_at")
-    list_display_links = ("id", "user")
-    search_fields = ("first_name", "last_name")
-    list_filter = ("user", "created_at", "updated_at")
+for model, prefix, fields in models_to_register:
+    @admin.register(model)
+    class ModelAdmin(admin.ModelAdmin):
+        list_display = (f"{prefix}id", f"{prefix}user", *fields)
+        list_display_links = (f"{prefix}id", f"{prefix}user")
+        search_fields = (f"{prefix}first_name", f"{prefix}last_name")
+        list_filter = (f"{prefix}user", "created_at", "updated_at")
