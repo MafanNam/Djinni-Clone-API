647c579435f9ac188bf347fe064c23dc5619973e
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:11 +0200

    improve

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -10,9 +10,14 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from environ import Env
 
-env = Env()
+# Set the DJANGO_SETTINGS_MODULE environment variable
+if "DJANGO_SETTINGS_MODULE" not in os.environ:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", env("DJANGO_SETTINGS_MODULE", default="backend.settings.local"))
+# Append the local settings module if it exists
+if os.path.exists(os.path.join(os.path.dirname(__file__), "settings", "local.py")):
+    os.environ["DJANGO_SETTINGS_MODULE"] += ".local"
+
+# Initialize the WSGI application
 application = get_wsgi_application()
