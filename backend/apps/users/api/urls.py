70433af24ea0736d49ebc1b84adb8db66d845d41
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:06 +0200

    improve

--- backend/apps/users/api/urls.py
+++ backend/apps/users/api/urls.py
@@ -1,14 +1,19 @@
 from django.urls import include, path, re_path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from . import views
 
 urlpatterns = [
     path("auth/", include("djoser.urls")),
     re_path(
-        r"^auth/jwt/create/?",
-        views.CustomTokenObtainPairView.as_view(),
+        r"^auth/jwt/create/?$",
+        TokenObtainPairView.as_view(),
         name="jwt-create",
     ),
-    path("auth/", include("djoser.urls.jwt")),
-    path("auth/", include("djoser.social.urls")),
+    path("auth/jwt/", include("djoser.urls.jwt")),
+    path("auth/social/", include("djoser.social.urls")),
 ]
+
+# Add this to your views.py file
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
