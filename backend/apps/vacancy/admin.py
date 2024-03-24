fea9dba7d9870a20961e8b4a52646cea174a24f7
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:07 +0200

    improve

--- backend/apps/vacancy/admin.py
+++ backend/apps/vacancy/admin.py
@@ -1,14 +1,14 @@
 from apps.vacancy.models import Feedback, Vacancy, VacancyView
 from django.contrib import admin
 
-
 @admin.register(VacancyView)
 class VacancyViewAdmin(admin.ModelAdmin):
     list_display = ["id", "vacancy", "user", "viewer_ip", "created_at", "updated_at"]
     list_display_links = ["id", "vacancy"]
     list_filter = ["created_at", "updated_at"]
     search_fields = ["vacancy", "user", "viewer_ip"]
-
+    readonly_fields = ["vacancy", "user", "viewer_ip", "created_at", "updated_at"]
+    date_hierarchy = 'created_at'
 
 @admin.register(Vacancy)
 class VacancyAdmin(admin.ModelAdmin):
@@ -16,7 +16,8 @@ class VacancyAdmin(admin.ModelAdmin):
     list_display_links = ["id", "user"]
     list_filter = ["created_at", "updated_at"]
     search_fields = ["user", "title", "company", "country"]
-
+    readonly_fields = ["user", "title", "company", "salary", "country", "created_at"]
+    date_hierarchy = 'created_at'
 
 @admin.register(Feedback)
 class FeedbackAdmin(admin.ModelAdmin):
@@ -24,3 +25,5 @@ class FeedbackAdmin(admin.ModelAdmin):
     list_display_links = ["id", "user"]
     list_filter = ["created_at", "updated_at"]
     search_fields = ["user", "vacancy"]
+    readonly_fields = ["user", "vacancy", "created_at"]
+    date_hierarchy = 'created_at'
