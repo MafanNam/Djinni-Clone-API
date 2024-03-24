0a7f1ec491c435fb255663ca986e2b0c89858096
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:01 +0200

    improve

--- backend/apps/other/api/urls.py
+++ backend/apps/other/api/urls.py
@@ -2,8 +2,3 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path("categories/", views.CategoryListAPIView.as_view()),
-    path("companies/", views.CompanyListCreateAPIView.as_view()),
-    path("companies/<int:pk>", views.CompanyDetailAPIView.as_view()),
-]
