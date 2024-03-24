9f706e5b7712e9110c62a2d84dc60b3ac58c5d48
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:08 +0200

    improve

--- backend/apps/vacancy/signals.py
+++ backend/apps/vacancy/signals.py
@@ -7,10 +7,17 @@ from .models import Feedback
 
 User = get_user_model()
 
-
 @receiver(post_save, sender=Feedback)
 def create_chatroom_and_message(sender, instance, created, **kwargs):
     """Create a chatroom and message when a new feedback is created."""
-    if created:
-        chat = ChatRoom.objects.create(candidate=instance.user, recruiter=instance.vacancy.user, feedback=instance)
-        ChatMessage.objects.create(user=instance.user, chat=chat, message=instance.cover_letter)
+    if created and instance.user and instance.vacancy and instance.vacancy.user:
+        chat = ChatRoom.objects.create(
+            candidate=instance.user,
+            recruiter=instance.vacancy.user,
+            feedback=instance
+        )
+        ChatMessage.objects.create(
+            user=instance.user,
+            chat=chat,
+            message=instance.cover_letter
+        )
