ee887a4ee320f58a5ef59466099af04934c9267e
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:13 +0200

    improve

--- backend/apps/users/apps.py
+++ backend/apps/users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
+import apps.users.signals  # import signals module here to avoid lazy initialization
 from django.utils.translation import gettext_lazy as _
-
 
 class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
@@ -8,4 +8,18 @@ class UsersConfig(AppConfig):
     verbose_name = _("Users")
 
     def ready(self):
-        import apps.users.signals
+        # Connect signals after all models have been loaded
+        from django.db import models
+        from django.contrib.auth.models import Group as AuthGroup
+        from django.contrib.contenttypes.models import ContentType
+
+        # Ensure User and AuthGroup models are loaded before initializing signals
+        models.load_model('auth.User')
+        models.load_model(AuthGroup)
+
+        apps.users.signals.connect_signals(
+            ContentType,
+            models.User,
+            AuthGroup
+        )
+
