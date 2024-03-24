0ad95c9f17ba17398bfb61ad180d0271af3317e1
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:01 +0200

    improve

--- backend/apps/other/api/serializers.py
+++ backend/apps/other/api/serializers.py
@@ -2,33 +2,8 @@ from apps.other.models import Category, Company
 from django_countries.serializer_fields import CountryField
 from rest_framework import serializers
 
-
-class CategorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Category
-        fields = ["id", "name"]
-
-
-class ShortCompanySerializer(serializers.ModelSerializer):
+class BaseCompanySerializer(serializers.ModelSerializer):
     country = CountryField(name_only=True)
 
     class Meta:
-        model = Company
-        fields = ["id", "name", "image", "country"]
 
-
-class CompanySerializer(serializers.ModelSerializer):
-    country = CountryField(name_only=True)
-
-    class Meta:
-        model = Company
-        fields = [
-            "id",
-            "name",
-            "image",
-            "bio",
-            "company_url",
-            "dou_url",
-            "country",
-            "num_employees",
-        ]
