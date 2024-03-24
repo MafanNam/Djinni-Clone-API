82b4872b93f9f54f7e678a7a1416bced392969c5
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:14 +0200

    improve

--- backend/backend/settings/local.py
+++ backend/backend/settings/local.py
@@ -1,55 +1,31 @@
-from .base import *  # noqa
 from .base import env
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY", default="django-insecure-kdxyc)gjt4qs-sk^##l54mrxczdv_)jm%_fg$)5_5bktcmt&ia")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG", default=True)
-
-# if DEBUG:
-#     # `debug` is only True in templates if the vistor IP is in INTERNAL_IPS.
-#     INTERNAL_IPS = type("c", (), {"__contains__": lambda *a: True})()
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG", default=False)
+INTERNAL_IPS = ["127.0.0.1", "0.0.0.0"]
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost:8080"]
 
-INSTALLED_APPS += [
+INSTALLED_APPS = [
     "debug_toolbar",
-]
-
-# MIDDLEWARE += [
-#     "apps.users.middleware.JWTFromCookieMiddleware",
-# ]
+] + INSTALLED_APPS
 
 MIDDLEWARE = ["debug_toolbar.middleware.DebugToolbarMiddleware"] + MIDDLEWARE
 
-# EMAIL
 EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "support@medium.site"
 DOMAIN = env("DOMAIN", default="localhost:8000")
 SITE_NAME = "Djinni Clone"
 
-# DEBUG TOOLBAR
 DEBUG_TOOLBAR_PANELS = [
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
     "debug_toolbar.panels.history.HistoryPanel",
     "debug_toolbar.panels.versions.VersionsPanel",
     "debug_toolbar.panels.timer.TimerPanel",
-    "debug_toolbar.panels.settings.SettingsPanel",
-    "debug_toolbar.panels.headers.HeadersPanel",
-    "debug_toolbar.panels.request.RequestPanel",
-    "debug_toolbar.panels.sql.SQLPanel",
-    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
-    "debug_toolbar.panels.templates.TemplatesPanel",
-    "debug_toolbar.panels.cache.CachePanel",
-    "debug_toolbar.panels.signals.SignalsPanel",
-    "cachalot.panels.CachalotPanel",
-]
-
-DEBUG_TOOLBAR_CONFIG = {
-    "SHOW_TOOLBAR_CALLBACK": lambda x: True,
-}
+    "debug_toolbar.panels
