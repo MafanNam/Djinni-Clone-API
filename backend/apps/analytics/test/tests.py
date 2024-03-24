478360fe77d58f2527a958da597da89e6f3ef656
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:02 +0200

    improve

--- backend/apps/analytics/test/tests.py
+++ backend/apps/analytics/test/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from myapp.views import HomePageView
 
-# Create your tests here.
+class HomePageViewTest(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_page_content(self):
+        response = self.client.get('/')
+        self.assertContains(response, "Welcome to my website!")
+
