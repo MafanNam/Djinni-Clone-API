f8403d83e1a862591761ebd03144d388c4935e1c
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:00 +0200

    improve

--- backend/apps/accounts/apps.py
+++ backend/apps/accounts/apps.py
@@ -1,8 +1,11 @@
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
-
 class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.accounts"
+    name = "your_project_name.apps.accounts" # added the project name to the name attribute
     verbose_name = _("Accounts")
+
+    def ready(self):
+        # Add any initialization code here, if needed
+        pass
