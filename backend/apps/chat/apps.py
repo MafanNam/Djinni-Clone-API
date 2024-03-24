35aa04a67a8ef319b340f4043ee3285ab24891cd
Maxim Manzik <68277193+MafanNam@users.noreply.github.com>
2024-03-24 16:54:00 +0200

    improve

--- backend/apps/chat/apps.py
+++ backend/apps/chat/apps.py
@@ -1,8 +1,3 @@
 from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as _
 
-
-class ChatConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.chat"
-    verbose_name = _("Chat")
